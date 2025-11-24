6307dfec49dbed990c37542a3f831a112b309008
govin-raaj <govin30raaj@gmail.com>
2025-11-24 14:30:58 +0530

    langgraph

--- app/data_processing/data_processing.py
+++ app/data_processing/data_processing.py
@@ -2,7 +2,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
 import os
 from typing import List
-from langchain.schema import Document
+from langchain_core.documents import Document
 from app.logger import logging
 
 class ProcessData:
